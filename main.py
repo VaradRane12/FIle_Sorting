9b93ea76f1a34fc3c1839d39711f8901483da226
Varad Rane <77780623+Izor12@users.noreply.github.com>
2024-06-01 12:31:21 +0530

    before dict

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import os,shutil
 import re
 path = r"D:\test_sorting/"
+full_name_ext = {}
 fl_lst = os.listdir(path)
-y = []
 for i in range(0,len(fl_lst)):
     if not os.path.isdir(path+fl_lst[i]):  #chekcing if the iterated file in a folder or a file appending only if its a file
-        y.append(os.path.splitext(fl_lst[i])[1])
+        full_name_ext[fl_lst[i]] = [os.path.splitext(fl_lst[i])[0],os.path.splitext(fl_lst[i])[1]]
 
-set(y)
+i = 0
+file =  open("file_types.txt")
+ext_lst = file.readlines()
+while i<len(ext_lst):
+    if (i%2 == 0):
+        for j in range(0,len(names)):
+            if(ext_lst[i].replace("\n","") in names[j]):
+                try:
+                    print("intial list",fl_lst[j+1])
+                    print("path:",ext_lst[i+1])
+                    print(path+fl_lst[j+1])
+                    shutil.move(path+fl_lst[j+1],ext_lst[i+1].replace("\n",""))
+                except:
+                    i+=1
+                    continue
+                
+        i+=1
+    else:
+        i+=1
+        continue
 
 
 for loop in range(0,len(y)):
@@ -25,3 +44,4 @@ for i in y:
                 continue
 
             
+            #CHANGE EVERYTHING TO THE NEW DICTIONARY
